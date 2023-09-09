ead0b857612ea8a0485145da46b67825f8207f48
tito1224 <64660812+tito1224@users.noreply.github.com>
2023-09-09 11:52:44 -0400

    Generate Sample Data
    Generate some sample data. Will refine data points when more of the UI has been completed.

---
+++ Utils/mktData.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import yfinance as yf
+
+# process to load data
+#data = yf.download("AMZN AAPL GOOG",start="2019-01-01",end="2020-01-01",interval="1d")["Adj Close"]
+#df = pd.DataFrame(data)
+#df.to_csv("sampledata.csv")
